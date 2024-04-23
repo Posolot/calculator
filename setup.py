a5c18930c4f284cde77362ecc5b31faa45a374e3
Posolot <138215725+Posolot@users.noreply.github.com>
2024-04-23 19:16:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Calculator',
+    name='calculator',
     version='1.0',
     description='Utility for performing simple mathematical operations',
     author='Ilya Kuzmin',
