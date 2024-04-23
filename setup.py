0683f0879f496853a29c86541eb3df53e82598c9
Posolot <138215725+Posolot@users.noreply.github.com>
2024-04-23 21:01:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='calculator',
-    version='1.0',
+    version='1.0.1',
     description='Utility for performing simple mathematical operations',
+    long_description='https://github.com/Posolot/calculator',
     author='Ilya Kuzmin',
     author_email='saharokbro@mail.ru',
     url='https://github.com/Posolot/calculator',
